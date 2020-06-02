e9e25edb38e66155a7aac6b5ca11af3e84844e30
Lucas Gonçalves <lucas.floripa@icloud.com>
2020-06-02 10:45:26 -0300

    first commit

---
+++ src/components/ui/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+
+function ElevationScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+function Header(props) {
+  return (
+    <ElevationScroll>
+      <AppBar position="fixed">
+        <Toolbar>Arc</Toolbar>
+      </AppBar>
+    </ElevationScroll>
+  );
+}
+
+export default Header;
